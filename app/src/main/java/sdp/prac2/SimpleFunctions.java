f35e8960c07fce2e291af8123798932eff893108
Zimvo Mazingisa <g23m1050@campus.ru.ac.za>
2025-04-10 15:44:43 +0200

    Task 1 Added

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -4,4 +4,15 @@ import java.util.*;
 
 public class SimpleFunctions {
     public SimpleFunctions() {}
+    public int Task1(int[] a, int[] b){
+        int adder = 0; //used for adding to
+        for (int i = 0; i < b.length; i++){ //for loop to go through the array that 
+            if(b[i]>=0 && b[i] < a.length){
+                adder = adder + a[b[i]];
+            }else{
+                adder = adder + 0;
+            }
+        }
+        return adder;
+    }
 }
