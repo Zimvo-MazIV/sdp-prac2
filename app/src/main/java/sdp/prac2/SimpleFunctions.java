e556594d40f31ed4f2384388538bdd006fa725d8
Zimvo Mazingisa <g23m1050@campus.ru.ac.za>
2025-04-12 02:08:10 +0200

    Task1 method added

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -7,8 +7,9 @@ public class SimpleFunctions {
     public int Task1(int[] a, int[] b){
         int adder = 0; //used for adding to
         for (int i = 0; i < b.length; i++){ //for loop to go through the array that 
-            if(b[i]>=0 && b[i] < a.length){
-                adder = adder + a[b[i]];
+            if(b[i]>=0 && b[i] < a.length){ //checks if the indexed value in b fits the range of values in array a.
+                adder = adder + a[b[i]]; //this line adds the previous number to the number array a 
+                //to the stipulated index in in array b
             }else{
                 adder = adder + 0;
             }
