54aea12119ef19fb02f6ea9aa779a259aa34d0cf
chriscan0tcode <166939941+chriscan0tcode@users.noreply.github.com>
2025-04-16 20:46:33 +0200

    Update AppTest.java
    I added the Task 4 test cases

--- app/src/test/java/sdp/prac2/AppTest.java
+++ app/src/test/java/sdp/prac2/AppTest.java
@@ -63,5 +63,65 @@ class AppTest {
 
         // Assert
         assertFalse(result);
+    
     }
+
+    public class Task4Test {
+
+    @Test
+    public void testNormalCase() {
+        List<Integer> a = Arrays.asList(1, 2, 3);
+        List<Integer> b = Arrays.asList(4, 5, 6);  // reversed: 6, 5, 4
+        List<Integer> expected = Arrays.asList(1 * 6, 2 * 5, 3 * 4);
+        assertEquals(expected, Task4(a, b));
+    }
+
+    @Test
+    public void testEmptyLists() {
+        List<Integer> a = new ArrayList<>();
+        List<Integer> b = new ArrayList<>();
+        List<Integer> expected = new ArrayList<>();
+        assertEquals(expected, Task4(a, b));
+    }
+
+    @Test
+    public void testSingleElementLists() {
+        List<Integer> a = Arrays.asList(7);
+        List<Integer> b = Arrays.asList(8);
+        List<Integer> expected = Arrays.asList(7 * 8);
+        assertEquals(expected, Task4(a, b));
+    }
+
+    @Test
+    public void testNegativeNumbers() {
+        List<Integer> a = Arrays.asList(-1, -2, 3);
+        List<Integer> b = Arrays.asList(4, -5, -6);
+        List<Integer> expected = Arrays.asList(-1 * -6, -2 * -5, 3 * 4);
+        assertEquals(expected, Task4(a, b));
+    }
+
+    @Test
+    public void testDifferentSizes() {
+        List<Integer> a = Arrays.asList(1, 2);
+        List<Integer> b = Arrays.asList(3, 4, 5);
+        assertNull(Task4(a, b));
+    }
+
+    // Reference to the method being tested
+    public static List<Integer> Task4(List<Integer> a, List<Integer> b) {
+        if (a.size() != b.size()) {
+            return null;
+        }
+
+        List<Integer> result = new ArrayList<>();
+        int n = a.size();
+
+        for (int i = 0; i < n; i++) {
+            int product = a.get(i) * b.get(n - 1 - i);
+            result.add(product);
+        }
+
+        return result;
+    }
+}
 }
